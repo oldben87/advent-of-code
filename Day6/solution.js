205bb6c35e74a499b7114d9d217d72a028367e37
Ben Webb <ben.webb@candide.eu>
2020-12-08 09:52:20 +0000

    day6 pt2

--- Day6/solution.js
+++ Day6/solution.js
@@ -1,12 +1,31 @@
 const fs = require('fs')
 const path = require('path')
+const { values } = require('ramda')
 
 // parse data to array
-const text = fs.readFileSync(path.resolve(__dirname, './dataTest.txt'), 'utf8')
-const data = text
-  .split('\n\n')
-  .map((answers) => answers.replace(/\n/g, '').split(''))
+const text = fs.readFileSync(path.resolve(__dirname, './data.txt'), 'utf8')
+const data = text.split('\n\n')
 
-const pt1 = data.map((yeses) => new Set(yeses).size).reduce((a, b) => a + b)
+const pt1 = data
+  .map((answers) => {
+    const yes = answers.replace(/\n/g, '').split('')
+    return new Set(yes).size
+  })
+  .reduce((a, b) => a + b)
+
+const pt2 = data
+  .map((answers) => {
+    const noPeople = answers.split(/\n/g).length
+    let charMap = {}
+    const yes = answers.replace(/\n/g, '').split('')
+    for (let char of yes) {
+      charMap[char] = charMap[char] + 1 || 1
+    }
+    const result = values(charMap).filter((a) => a === noPeople).length
+
+    return result
+  })
+  .reduce((a, b) => a + b)
 
 console.log('pt1', pt1)
+console.log('pt2', pt2)
