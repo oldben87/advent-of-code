1b6eac2d62e8b305828033b37a41de38479081f9
Ben Webb <ben.webb@candide.eu>
2020-12-13 08:36:08 +0000

    add day 9 pt2

--- Day9/solution.js
+++ Day9/solution.js
@@ -1,14 +1,15 @@
 const fs = require('fs')
 const path = require('path')
-const { slice } = require('ramda')
+const { slice, splitAt, equals, tail } = require('ramda')
 
 // parse data
 const text = fs.readFileSync(path.resolve(__dirname, './data.txt'), 'utf8')
 const data = text.split('\n').map((num) => parseInt(num))
-console.log(data)
-
+const dataCopy = data.slice(0)
 function sumLast25(arr) {
+  //change below based on input
   const numberToSum = 25
+
   const prevItems = slice(0, numberToSum)(arr)
 
   for (let a = 0; a <= prevItems.length - 1; a++) {
@@ -28,4 +29,28 @@ function sumLast25(arr) {
   }
 }
 
-console.log(sumLast25(data))
+// funtction 2; split data at sumLast25
+// set number to sum
+const pt1 = sumLast25(dataCopy)
+const dataSplit = splitAt(data.findIndex(equals(pt1)))(data)
+
+function calculateSumRange(arr, matchingValue) {
+  let sumIndex
+  arr.reduce((acc, val, ind) => {
+    if (acc === matchingValue) {
+      sumIndex = ind
+    }
+    return acc + val
+  }, 0)
+
+  if (sumIndex) {
+    const getLargest = arr.slice(0, sumIndex + 1).sort()
+    return arr[0] + getLargest.pop()
+  } else {
+    arr.shift()
+    return calculateSumRange(arr, matchingValue)
+  }
+}
+
+console.log('pt1', pt1)
+console.log('pt2', calculateSumRange(dataSplit[0], pt1))
