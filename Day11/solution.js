c5013152db2c660599fef2d6a9f7b35e3b7708e0
Ben Webb <ben.webb@candide.eu>
2020-12-16 15:24:22 +0000

    adding day11 pt2

--- Day11/solution.js
+++ Day11/solution.js
@@ -50,4 +50,132 @@ function seatSwap(arr) {
     : seatSwap(newArr)
 }
 
+function seatSwap2(arr) {
+  let newArr = [...arr]
+  arr.forEach((row, rowInd) => {
+    row.split('').forEach((col, colInd) => {
+      let occupied = 0
+      // north
+      for (let i = 1; i <= arr.length - 1; i++) {
+        if (!arr[rowInd - i] || arr[rowInd - i][colInd] === 'L') {
+          break
+        }
+        if (arr[rowInd - i][colInd] === '#') {
+          occupied++
+          break
+        }
+      }
+
+      //north east
+      for (let i = 1; i <= arr.length - 1; i++) {
+        if (
+          !arr[rowInd - i] ||
+          !arr[rowInd - i][colInd + i] ||
+          arr[rowInd - i][colInd + i] === 'L'
+        ) {
+          break
+        }
+        if (arr[rowInd - i][colInd + i] === '#') {
+          occupied++
+          break
+        }
+      }
+
+      //east
+      for (let i = 1; i <= arr[rowInd].length - 1; i++) {
+        if (!arr[rowInd][colInd + i] || arr[rowInd][colInd + i] === 'L') {
+          break
+        }
+        if (arr[rowInd][colInd + i] === '#') {
+          occupied++
+          break
+        }
+      }
+
+      //south east
+      for (let i = 1; i <= arr.length - 1; i++) {
+        if (
+          !arr[rowInd + i] ||
+          !arr[rowInd + i][colInd + i] ||
+          arr[rowInd + i][colInd + i] === 'L'
+        ) {
+          break
+        }
+        if (arr[rowInd + i][colInd + i] === '#') {
+          occupied++
+          break
+        }
+      }
+      //south
+      for (let i = 1; i <= arr.length - 1; i++) {
+        if (!arr[rowInd + i] || arr[rowInd + i][colInd] === 'L') {
+          break
+        }
+        if (arr[rowInd + i][colInd] === '#') {
+          occupied++
+          break
+        }
+      }
+
+      //south west
+      for (let i = 1; i <= arr.length - 1; i++) {
+        if (
+          !arr[rowInd + i] ||
+          !arr[rowInd + i][colInd - i] ||
+          arr[rowInd + i][colInd - i] === 'L'
+        ) {
+          break
+        }
+        if (arr[rowInd + i][colInd - i] === '#') {
+          occupied++
+          break
+        }
+      }
+
+      // west
+      for (let i = 1; i <= arr[rowInd].length - 1; i++) {
+        if (!arr[rowInd][colInd - i] || arr[rowInd][colInd - i] === 'L') {
+          break
+        }
+        if (arr[rowInd][colInd - i] === '#') {
+          occupied++
+          break
+        }
+      }
+
+      // north west
+      for (let i = 1; i <= arr.length - 1; i++) {
+        if (
+          !arr[rowInd - i] ||
+          !arr[rowInd - i][colInd - i] ||
+          arr[rowInd - i][colInd - i] === 'L'
+        ) {
+          break
+        }
+        if (arr[rowInd - i][colInd - i] === '#') {
+          occupied++
+          break
+        }
+      }
+
+      if (occupied === 0 && arr[rowInd][colInd] === 'L') {
+        const split = newArr[rowInd].split('')
+        split[colInd] = '#'
+        newArr[rowInd] = split.join('')
+      }
+
+      if (occupied >= 5 && arr[rowInd][colInd] === '#') {
+        const split = newArr[rowInd].split('')
+        split[colInd] = 'L'
+        newArr[rowInd] = split.join('')
+      }
+    })
+  })
+  return newArr.toString() === arr.toString()
+    ? countOccupied(arr)
+    : seatSwap2(newArr)
+}
+
 console.log('pt1', seatSwap(data))
+
+console.log('pt2', seatSwap2(data))
