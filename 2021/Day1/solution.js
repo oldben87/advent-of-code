e44a3e05b946dd0142f3c4711e5218e746ce8444
Ben Webb <ben@Bens-MacBook-Air.local>
2021-12-06 09:26:16 +0000

    Add Day1 pt2 solution

--- 2021/Day1/solution.js
+++ 2021/Day1/solution.js
@@ -5,9 +5,8 @@ const path = require("path");
 const text = fs.readFileSync(path.resolve(__dirname, "./data.txt"), "utf8");
 const data = text.split("\n").map((num) => parseInt(num));
 
-const getDepthIncreaseCount = (input) => {
-  console.log(input);
-  return input.reduce((acc, item, index, array) => {
+const getDepthIncreaseCount = (array) => {
+  return array.reduce((acc, item, index, array) => {
     return item > array[index - 1] ? acc + 1 : acc;
   }, 0);
 };
@@ -15,3 +14,19 @@ const getDepthIncreaseCount = (input) => {
 console.time("pt1");
 console.log("pt1", getDepthIncreaseCount(data));
 console.timeEnd("pt1");
+
+const mergeEveryThreeDepths = (numArr) => {
+  const merged = numArr.map((num, ind, arr) => {
+    if (arr[ind + 1] != null && arr[ind + 2] != null) {
+      return num + arr[ind + 1] + arr[ind + 2];
+    } else return null;
+  });
+
+  const filteredMerge = merged.filter((number) => number != null);
+
+  return filteredMerge;
+};
+
+console.time("pt2");
+console.log("pt2", getDepthIncreaseCount(mergeEveryThreeDepths(data)));
+console.timeEnd("pt2");
