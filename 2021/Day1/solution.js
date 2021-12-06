220f33a5379259e46b1c2fe68fce29eec3f5ef4f
Ben Webb <b-webb@outlook.com>
2021-12-06 16:07:10 +0000

    Remove semi colons

--- 2021/Day1/solution.js
+++ 2021/Day1/solution.js
@@ -1,32 +1,32 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require("fs")
+const path = require("path")
 
 // parse data to array
-const text = fs.readFileSync(path.resolve(__dirname, "./data.txt"), "utf8");
-const data = text.split("\n").map((num) => parseInt(num));
+const text = fs.readFileSync(path.resolve(__dirname, "./data.txt"), "utf8")
+const data = text.split("\n").map((num) => parseInt(num))
 
 const getDepthIncreaseCount = (array) => {
   return array.reduce((acc, item, index, array) => {
-    return item > array[index - 1] ? acc + 1 : acc;
-  }, 0);
-};
+    return item > array[index - 1] ? acc + 1 : acc
+  }, 0)
+}
 
-console.time("pt1");
-console.log("pt1", getDepthIncreaseCount(data));
-console.timeEnd("pt1");
+console.time("pt1")
+console.log("pt1", getDepthIncreaseCount(data))
+console.timeEnd("pt1")
 
 const mergeEveryThreeDepths = (numArr) => {
   const merged = numArr.map((num, ind, arr) => {
     if (arr[ind + 1] != null && arr[ind + 2] != null) {
-      return num + arr[ind + 1] + arr[ind + 2];
-    } else return null;
-  });
+      return num + arr[ind + 1] + arr[ind + 2]
+    } else return null
+  })
 
-  const filteredMerge = merged.filter((number) => number != null);
+  const filteredMerge = merged.filter((number) => number != null)
 
-  return filteredMerge;
-};
+  return filteredMerge
+}
 
-console.time("pt2");
-console.log("pt2", getDepthIncreaseCount(mergeEveryThreeDepths(data)));
-console.timeEnd("pt2");
+console.time("pt2")
+console.log("pt2", getDepthIncreaseCount(mergeEveryThreeDepths(data)))
+console.timeEnd("pt2")
--- 2021/Day2/solution.js
+++ 2021/Day2/solution.js
@@ -1,57 +1,57 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require("fs")
+const path = require("path")
 
 // parse data to array
-const text = fs.readFileSync(path.resolve(__dirname, "./data.txt"), "utf8");
+const text = fs.readFileSync(path.resolve(__dirname, "./data.txt"), "utf8")
 const data = text.split("\n").map((cmd) => {
-  const [instruction, value] = cmd.split(" ");
+  const [instruction, value] = cmd.split(" ")
 
-  return { instruction, value: parseInt(value, 10) };
-});
+  return { instruction, value: parseInt(value, 10) }
+})
 
 const calculateFinalPosition = (commands) => {
-  let horizontal = 0;
-  let depth = 0;
+  let horizontal = 0
+  let depth = 0
 
   for (let command of commands) {
     if (command.instruction === "forward") {
-      horizontal += command.value;
+      horizontal += command.value
     }
     if (command.instruction === "down") {
-      depth += command.value;
+      depth += command.value
     }
     if (command.instruction === "up") {
-      depth -= command.value;
+      depth -= command.value
     }
   }
-  return horizontal * depth;
-};
+  return horizontal * depth
+}
 
 const calculatePositionUsingAim = (commands) => {
-  let aim = 0;
-  let depth = 0;
-  let horizontal = 0;
+  let aim = 0
+  let depth = 0
+  let horizontal = 0
 
   for (let command of commands) {
-    const { instruction, value } = command;
+    const { instruction, value } = command
     if (instruction === "forward") {
-      horizontal += value;
-      depth += aim * value;
+      horizontal += value
+      depth += aim * value
     }
     if (instruction === "down") {
-      aim += value;
+      aim += value
     }
     if (instruction === "up") {
-      aim -= value;
+      aim -= value
     }
   }
-  return horizontal * depth;
-};
+  return horizontal * depth
+}
 
-console.time("pt1");
-console.log("pt1", calculateFinalPosition(data));
-console.timeEnd("pt1");
+console.time("pt1")
+console.log("pt1", calculateFinalPosition(data))
+console.timeEnd("pt1")
 
-console.time("pt2");
-console.log("pt2", calculatePositionUsingAim(data));
-console.timeEnd("pt2");
+console.time("pt2")
+console.log("pt2", calculatePositionUsingAim(data))
+console.timeEnd("pt2")
