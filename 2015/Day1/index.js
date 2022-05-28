c30b7d354ee1eefc916562902dc87f4105d99af7
Ben Webb <b-webb@outlook.com>
2022-05-28 17:01:17 +0100

    2015 - Day 1

---
+++ 2015/Day1/index.js
@@ -0,0 +1,47 @@
+const fs = require("fs")
+const path = require("path")
+
+// parse data to array
+const text = fs.readFileSync(path.resolve(__dirname, "./data.txt"), "utf8")
+
+console.log(text)
+
+const floorNumber = (instructions) => {
+  return instructions.split("").reduce((floor, inst) => {
+    if (inst === "(") {
+      return floor + 1
+    }
+    return floor - 1
+  }, 0)
+}
+
+const instructionForBasement = (instructions) => {
+  return instructions.split("").reduce(
+    (res, inst, index) => {
+      if (res.firstBasement !== undefined) {
+        return res
+      }
+
+      let newFloor
+      if (inst === "(") {
+        newFloor = res.floor + 1
+      } else {
+        newFloor = res.floor - 1
+      }
+
+      return {
+        floor: newFloor,
+        firstBasement: newFloor === -1 ? index + 1 : undefined,
+      }
+    },
+    { floor: 0, firstBasement: undefined }
+  )
+}
+
+console.time("pt1")
+console.log("pt1", floorNumber(text))
+console.timeEnd("pt1")
+
+console.time("pt2")
+console.log("pt2", instructionForBasement(text))
+console.timeEnd("pt2")
