d40f4ff5b3df34ebb4f1d9200a9aa28ce138c16c
Ben Webb <b-webb@outlook.com>
2022-12-02 09:40:33 +0000

    Add day 2 solution

--- 2022/Day2/solution.js
+++ 2022/Day2/solution.js
@@ -3,7 +3,6 @@ const path = require("path")
 
 // parse data to array
 console.time("pt1")
-// console.time("pt2")
 const text = fs.readFileSync(path.resolve(__dirname, "./input.txt"), "utf8")
 
 /* 
@@ -23,9 +22,42 @@ const choiceScore = {
   Z: 3,
 }
 
-const data = text.split("\n").reduce((score, moves) => {
+const pt1 = text.split("\n").reduce((score, moves) => {
   const [opponent, myMove] = moves.split(" ")
   return score + moveScoreMaps[opponent][myMove] + choiceScore[myMove]
 }, 0)
-console.log("pt1", data)
+console.log("pt1", pt1)
 console.timeEnd("pt1")
+
+const choiceScorePT2 = {
+  ROCK: 1,
+  PAPER: 2,
+  SCISSORS: 3,
+}
+
+const moveScoreMapsPT2 = {
+  A: { ROCK: 3, PAPER: 6, SCISSORS: 0 },
+  B: { ROCK: 0, PAPER: 3, SCISSORS: 6 },
+  C: { ROCK: 6, PAPER: 0, SCISSORS: 3 },
+}
+
+const winDrawLoseMap = {
+  X: 0,
+  Y: 3,
+  Z: 6,
+}
+
+const pt2 = text.split("\n").reduce((score, moves) => {
+  const [opponent, myAction] = moves.split(" ")
+  const winDrawLose = winDrawLoseMap[myAction]
+
+  const [myChoice, roundScore] = Object.entries(
+    moveScoreMapsPT2[opponent]
+  ).find(([_, score]) => score === winDrawLose)
+
+  return score + roundScore + choiceScorePT2[myChoice]
+}, 0)
+
+console.time("pt2")
+console.log("pt2", pt2)
+console.timeEnd("pt2")
