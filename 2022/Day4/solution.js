ecb5e0d214d78625e6a655fda9a129882776d89c
Ben Webb <b-webb@outlook.com>
2022-12-04 13:04:02 +0000

    Add day 4 part 2 solution

--- 2022/Day4/solution.js
+++ 2022/Day4/solution.js
@@ -1,6 +1,5 @@
 const fs = require("fs")
 const path = require("path")
-const { splitAt, splitEvery } = require("ramda")
 
 // parse data to array
 const text = fs.readFileSync(path.resolve(__dirname, "./input.txt"), "utf8")
@@ -30,6 +29,21 @@ console.log(
 )
 console.timeEnd("pt1")
 
+const getTaskOverlap = (elf1, elf2) => {
+  const [elf1Start, elf1End] = parseElfSections(elf1)
+  const [elf2Start, elf2End] = parseElfSections(elf2)
+
+  if (elf1Start <= elf2End && elf2Start <= elf1End) return true
+
+  return false
+}
+
 console.time("pt2")
-console.log("pt2")
+console.log(
+  "pt2",
+  parseInput(text).reduce((total, [elf1, elf2]) => {
+    const isOverlapping = getTaskOverlap(elf1, elf2)
+    return isOverlapping ? total + 1 : total
+  }, 0)
+)
 console.timeEnd("pt2")
