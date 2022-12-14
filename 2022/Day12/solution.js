32adf375f483d6ef02423f4c2b223f4027452665
Ben Webb <b-webb@outlook.com>
2022-12-14 08:41:42 +0000

    Add day 12 part 2 solution

--- 2022/Day12/solution.js
+++ 2022/Day12/solution.js
@@ -35,11 +35,13 @@ const findStartAndEnd = (map) => {
   return { start, end }
 }
 
+const getMemoKey = (x, y) => `${x}-${y}`
+
 const getDistanceToEnd = (x, y, map, visited, moveCount) => {
   if (map[x] === undefined || map[x][y] === undefined) {
     return
   }
-  const memoKey = `${x}-${y}`
+  const memoKey = getMemoKey(x, y)
 
   if (map[x][y] === "E") {
     if (visited[memoKey] === undefined || visited[memoKey] > moveCount) {
@@ -109,22 +111,55 @@ const getDistanceToEnd = (x, y, map, visited, moveCount) => {
   }
 }
 
+const findAllA = (map) => {
+  const aLocations = []
+  map.forEach((row, rowIndex) => {
+    row.forEach((cell, colIndex) => {
+      if (cell === 0) {
+        aLocations.push({ x: rowIndex, y: colIndex })
+      }
+    })
+  })
+  return aLocations
+}
+
 const getShortestRoute = (map) => {
-  const { start, end } = findStartAndEnd(map)
-
-  const [startX, startY] = start
-
+  const {
+    start: [startX, startY],
+    end: [endX, endY],
+  } = findStartAndEnd(map)
   const visitedMemo = {}
 
   getDistanceToEnd(startX, startY, map, visitedMemo, 0)
 
-  return visitedMemo[`${end[0]}-${end[1]}`]
+  const memoKey = getMemoKey(endX, endY)
+
+  return visitedMemo[memoKey]
+}
+
+const findClosestA = (map) => {
+  const {
+    end: [endX, endY],
+  } = findStartAndEnd(map)
+  const distanceToA = []
+  const visitedMemo = {}
+
+  const memoKey = getMemoKey(endX, endY)
+
+  const aCoords = findAllA(map)
+  aCoords.forEach(({ x, y }) => {
+    getDistanceToEnd(x, y, map, visitedMemo, 0)
+
+    distanceToA.push(visitedMemo[memoKey])
+  })
+
+  return Math.min(...distanceToA)
 }
 
 console.time("pt1")
 console.log("pt1", getShortestRoute(input))
 console.timeEnd("pt1")
 
-// console.time("pt2")
-// console.log("pt2")
-// console.timeEnd("pt2")
+console.time("pt2")
+console.log("pt2", findClosestA(input)) // Start at the end and find the shortest to have an a?
+console.timeEnd("pt2")
