d47bdbcab07d8effab6e517ed12a9af2627d47e8
Ben Webb <b-webb@outlook.com>
2022-12-19 10:47:38 +0000

    Add day 13 pt2

--- 2022/Day13/solution.js
+++ 2022/Day13/solution.js
@@ -57,16 +57,75 @@ const sumValidPairIndexes = (pairs) => {
   pairs.forEach(({ first, second }, index) => {
     if (comparePairs(first, second)) {
       validIndexSums += index + 1
-      console.log({ first, second, index: index + 1 })
     }
   })
   return validIndexSums
 }
 
-console.time("pt1")
-console.log("pt1", sumValidPairIndexes(parseInput(input)))
-console.timeEnd("pt1")
+// console.time("pt1")
+// console.log("pt1", sumValidPairIndexes(parseInput(input)))
+// console.timeEnd("pt1")
 
-// console.time("pt2")
-// console.log("pt2")
-// console.timeEnd("pt2")
+const getIndexOfNewPairs = (pairs) => {
+  const parsedInput = parseInput(pairs)
+  const newArray = parsedInput.reduce((newArray, { first, second }) => {
+    return [...newArray, first, second]
+  }, [])
+  newArray.push([[2]])
+  newArray.push([[6]])
+
+  const sortedPairs = newArray.sort((a, b) => {
+    const compared = comparePairs(a, b)
+    if (compared) {
+      return -1
+    }
+    if (compared === false) {
+      return 1
+    }
+    return 0
+  })
+
+  const indexOf2 =
+    sortedPairs.findIndex((current) => {
+      if (!current) return false
+      if (Array.isArray(current)) {
+        if (Array.isArray(current[0])) {
+          return (
+            current &&
+            current.length === 1 &&
+            current[0].length === 1 &&
+            current[0][0] === 2
+          )
+        }
+      }
+      return false
+    }) + 1
+
+  const indexOf6 =
+    sortedPairs.findIndex((current) => {
+      if (!current) return false
+      if (Array.isArray(current)) {
+        if (Array.isArray(current[0])) {
+          return (
+            current &&
+            current.length === 1 &&
+            current[0].length === 1 &&
+            current[0][0] === 6
+          )
+        }
+      }
+      return false
+    }) + 1
+
+  return indexOf2 * indexOf6
+}
+
+// make an array with all pairs in.
+
+// use compare function to sort array
+
+// find indexes (index + 1) of [[2]] * [[6]]
+
+console.time("pt2")
+console.log("pt2", getIndexOfNewPairs(input))
+console.timeEnd("pt2")
