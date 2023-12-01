ad24141a02b1c192067efcc71d8183afb491fddb
Ben Webb <b-webb@outlook.com>
2023-12-01 10:56:27 +0000

    Day 15 2022 - WIP

--- 2022/Day15/solution.js
+++ 2022/Day15/solution.js
@@ -66,10 +66,71 @@ const countSensorDeadZone = (sensors, rowToCount) => {
   return res.size
 }
 
-console.time("pt1")
-console.log("pt1", countSensorDeadZone(input, 2000000))
-console.timeEnd("pt1")
+// console.time("pt1")
+// console.log("pt1", countSensorDeadZone(input, 2000000))
+// console.timeEnd("pt1")
 
-// console.time("pt2")
-// console.log("pt2")
-// console.timeEnd("pt2")
+// find a coordinate that is not in the coord list.
+//
+
+const getEmptyCoordinate = (sensors, maxPosition) => {
+  const parsedSensors = parseInput(sensors)
+
+  const beaconList = parsedSensors.map(({ beacon: { x, y } }) =>
+    coordString(x, y)
+  )
+
+  let coordinates = []
+
+  for (let x = 0; x <= maxPosition; x++) {
+    if (coordinates.length > 0) {
+      break
+    }
+    for (let y = 0; y <= maxPosition; y++) {
+      if (coordinates.length > 0) {
+        break
+      }
+      const coords = coordString(x, y)
+      if (beaconList.includes(coords)) {
+        continue
+      }
+
+      const position = { x, y }
+      let isInvalid = false
+      parsedSensors.forEach((sensor) => {
+        if (isInvalid) {
+          return
+        }
+
+        const distance = getManhattenDistance(sensor, position)
+
+        if (distance <= sensor.distance) {
+          isInvalid = true
+        }
+      })
+      if (!isInvalid) {
+        coordinates.push({ x, y })
+      }
+    }
+  }
+  return coordinates[0].x * 4000000 + coordinates[0].y
+}
+
+// count edges of the sensors.
+// make ranges
+
+const getRanges = () => {}
+const findEmptyCoordinate = (data, finalPosition) => {
+  const parsedSensors = parseInput(data)
+
+  // create map of sensor positions
+  const rangesMapping = {}
+
+  parsedSensors.forEach((sens) => {})
+
+  return sensorMapping
+}
+
+console.time("pt2")
+console.log("pt2", findEmptyCoordinate(input, 4000000))
+console.timeEnd("pt2")
